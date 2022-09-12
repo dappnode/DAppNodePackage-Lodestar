62a8128bbbcd9711d2b87d5fe4707fd1fd6f80fd
Nabsku <12911176+Nabsku@users.noreply.github.com>
2022-09-12 14:46:15 +0200

    Made Lodestar mainnet ready

--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -1,16 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 exec node /usr/app/node_modules/.bin/lodestar \
     validator \
-    --preset=gnosis \
-    --paramsFile=/usr/config.yaml \
-    --defaultFeeRecipient ${FEE_RECIPIENT} \
-    --graffiti ${GRAFFITI} \
-    --rootDir /var/lib/data \
-    --keymanagerEnabled \
-    --server http://beacon-chain.lodestar-chiado.dappnode:4000 \
-    --logLevelFile debug \
+    --network=mainnet \
+    --suggestedFeeRecipient=${FEE_RECIPIENT_ADDRESS} \
+    --graffiti=${GRAFFITI} \
+    --externalSigner.url=${HTTP_WEB3SIGNER} \
+    --externalSigner.fetch \
+    --doppelgangerProtectionEnabled \
+    --server=${BEACON_NODE_ADDR} \
+    --logLevel=${DEBUG_LEVEL} \
+    --logFileLevel=${DEBUG_LEVEL} \
     --logFile /var/lib/data/validator.log \
-    --logRotate \
-    --logMaxFiles 5 \
     $EXTRA_OPTS
