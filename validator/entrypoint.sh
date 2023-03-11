303dd69db5d71008323447fa87613dcfccdd79d0
nflaig <nflaig@protonmail.com>
2023-03-11 12:27:33 +0100

    Update validator entrypoint based on prater package

--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 
-# MEVBOOST: https://chainsafe.github.io/lodestar/usage/mev-integration/
+# MEV-Boost: https://chainsafe.github.io/lodestar/usage/mev-integration/
 if [ -n "$_DAPPNODE_GLOBAL_MEVBOOST_MAINNET" ] && [ "$_DAPPNODE_GLOBAL_MEVBOOST_MAINNET" == "true" ]; then
-  echo "MEV-Boost is enabled"
-  MEVBOOST_URL="http://mev-boost.mev-boost.dappnode:18550"
-  if curl --retry 5 --retry-delay 5 --retry-all-errors "${MEVBOOST_URL}"; then
-    EXTRA_OPTS="--builder ${EXTRA_OPTS}"
-  else
-    echo "MEV-Boost is enabled but the Mainnet MEV-Boost package at ${MEVBOOST_URL} is not reachable"
-    curl -X POST -G 'http://my.dappnode/notification-send' --data-urlencode 'type=danger' --data-urlencode title="${MEVBOOST_URL} can not be reached" --data-urlencode 'body=Make sure the Mainnet MEV-Boost DNP is available and running'
-  fi
+    echo "MEV-Boost is enabled"
+    MEVBOOST_URL="http://mev-boost.mev-boost.dappnode:18550"
+    if curl --retry 5 --retry-delay 5 --retry-all-errors "${MEVBOOST_URL}"; then
+        EXTRA_OPTS="--builder ${EXTRA_OPTS}"
+    else
+        echo "MEV-Boost is enabled but the Mainnet MEV-Boost package at ${MEVBOOST_URL} is not reachable"
+        curl -X POST -G 'http://my.dappnode/notification-send' --data-urlencode 'type=danger' --data-urlencode title="${MEVBOOST_URL} can not be reached" --data-urlencode 'body=Make sure the Mainnet MEV-Boost DNP is available and running'
+    fi
 fi
+
+# Handle Graffiti Character Limit
+oLang=$LANG oLcAll=$LC_ALL
+LANG=C LC_ALL=C 
+graffitiString=${GRAFFITI:0:32}
+LANG=$oLang LC_ALL=$oLcAll
 
 exec node /usr/app/node_modules/.bin/lodestar \
     validator \
     --network=mainnet \
     --suggestedFeeRecipient=${FEE_RECIPIENT_ADDRESS} \
-    --graffiti=${GRAFFITI:0:32} \
+    --graffiti="${graffitiString}" \
     --dataDir=/var/lib/data \
     --keymanager true \
     --keymanager.authEnabled true \
@@ -30,5 +36,6 @@ exec node /usr/app/node_modules/.bin/lodestar \
     --beaconNodes=${BEACON_NODE_ADDR} \
     --logLevel=${DEBUG_LEVEL} \
     --logFileLevel=${DEBUG_LEVEL} \
+    --logFileDailyRotate 5 \
     --logFile /var/lib/data/validator.log \
     $EXTRA_OPTS
