cd5333a9be81c9cf9474a2a6fe7761417bf8a74f
nflaig <nflaig@protonmail.com>
2023-03-14 09:53:15 +0100

    Move mev-boost health check to beacon node entrypoint script
    Only the beacon node needs to communicate with the builder (mev-boost)
    and in case mev-boost is not running we only want to send one notification
    to the user.
    
    See https://github.com/dappnode/DAppNodePackage-Lodestar-Prater/pull/52#discussion_r1133072650

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -26,7 +26,12 @@ esac
 if [ -n "$_DAPPNODE_GLOBAL_MEVBOOST_MAINNET" ] && [ "$_DAPPNODE_GLOBAL_MEVBOOST_MAINNET" == "true" ]; then
     echo "MEV-Boost is enabled"
     MEVBOOST_URL="http://mev-boost.mev-boost.dappnode:18550"
-    EXTRA_OPTS="${EXTRA_OPTS} --builder --builder.urls=${MEVBOOST_URL}"
+    if curl --retry 5 --retry-delay 5 --retry-all-errors "${MEVBOOST_URL}"; then
+        EXTRA_OPTS="--builder --builder.urls=${MEVBOOST_URL} ${EXTRA_OPTS}"
+    else
+        echo "MEV-Boost is enabled but the Mainnet MEV-Boost package at ${MEVBOOST_URL} is not reachable"
+        curl -X POST -G 'http://my.dappnode/notification-send' --data-urlencode 'type=danger' --data-urlencode title="${MEVBOOST_URL} can not be reached" --data-urlencode 'body=Make sure the Mainnet MEV-Boost DNP is available and running'
+    fi
 fi
 
 exec node /usr/app/node_modules/.bin/lodestar \
