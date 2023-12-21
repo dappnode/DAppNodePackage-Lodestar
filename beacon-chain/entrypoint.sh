7fafeb2da50a41a6ec15113d4c4708ae7af1a241
Nico Flaig <nflaig@protonmail.com>
2023-12-21 12:02:47 +0100

    Update flag to pass builder url

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -27,7 +27,7 @@ if [ -n "$_DAPPNODE_GLOBAL_MEVBOOST_MAINNET" ] && [ "$_DAPPNODE_GLOBAL_MEVBOOST_
     echo "MEV-Boost is enabled"
     MEVBOOST_URL="http://mev-boost.mev-boost.dappnode:18550"
     if curl --retry 5 --retry-delay 5 --retry-all-errors "${MEVBOOST_URL}"; then
-        EXTRA_OPTS="--builder --builder.urls=${MEVBOOST_URL} ${EXTRA_OPTS}"
+        EXTRA_OPTS="--builder --builder.url=${MEVBOOST_URL} ${EXTRA_OPTS}"
     else
         echo "MEV-Boost is enabled but the Mainnet MEV-Boost package at ${MEVBOOST_URL} is not reachable"
         curl -X POST -G 'http://my.dappnode/notification-send' --data-urlencode 'type=danger' --data-urlencode title="${MEVBOOST_URL} can not be reached" --data-urlencode 'body=Make sure the Mainnet MEV-Boost DNP is available and running'
